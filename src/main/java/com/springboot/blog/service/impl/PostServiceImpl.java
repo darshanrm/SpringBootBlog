cc8ae2aaf1a455ee5a4554d5f5c59d5144844b97
darshanrm <darshan.marathe13@gmail.com>
2022-01-01 22:07:10 +0530

    PAGINATION AND SORTING

---
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -0,0 +1,110 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.exception.ResourceNotFoundException;
+import com.springboot.blog.model.Post;
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.payload.PostResponse;
+import com.springboot.blog.repository.PostRepositiory;
+import com.springboot.blog.service.PostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepositiory postRepositiory;
+
+    public PostServiceImpl(PostRepositiory postRepositiory) {
+        this.postRepositiory = postRepositiory;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+
+        //convert DTO to model
+        Post post = mapToModel(postDto);
+
+        Post newPost = postRepositiory.save(post);
+
+        //convert model to DTO
+        PostDto postResponse = mapToDto(newPost);
+
+        return postResponse;
+    }
+
+    @Override
+    public PostResponse getAllPosts(int pageNo,int pageSize, String sortBy, String sortDir) {
+
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+        //create pagination and sorting
+        Pageable pageable = PageRequest.of(pageNo,pageSize, sort);
+        Page<Post> posts = postRepositiory.findAll(pageable);
+
+        //get content from page
+        List<Post> listOfPosts = posts.getContent();
+
+        List<PostDto> allPosts = new ArrayList<>();
+        for(Post post:listOfPosts){
+            PostDto obj = mapToDto(post);
+            allPosts.add(obj);
+        }
+
+        PostResponse postResponse = new PostResponse();
+        postResponse.setContent(allPosts);
+        postResponse.setPageNo(posts.getNumber());
+        postResponse.setPageSize(posts.getSize());
+        postResponse.setTotalElements(posts.getTotalElements());
+        postResponse.setTotalPages(posts.getTotalPages());
+        postResponse.setLast(posts.isLast());
+
+        return postResponse;
+    }
+
+    @Override
+    public PostDto getPostById(long id) {
+        Post post = postRepositiory.findById(id).orElseThrow(()-> new ResourceNotFoundException("Post","id",id));
+        return mapToDto(post);
+    }
+
+    @Override
+    public PostDto updatePost(PostDto postDto, long id) {
+        Post post = postRepositiory.findById(id).orElseThrow(()-> new ResourceNotFoundException("Post","id",id));
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post updatedPost = postRepositiory.save(post);
+        return mapToDto(updatedPost);
+    }
+
+    @Override
+    public void deletePost(long id) {
+        Post post = postRepositiory.findById(id).orElseThrow(()->new ResourceNotFoundException("Post","id",id));
+        postRepositiory.deleteById(id);
+    }
+
+    //convert model to PostDto
+    private PostDto mapToDto(Post post){
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+        return postDto;
+    }
+
+    private Post mapToModel(PostDto postDto){
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        return post;
+    }
+}
